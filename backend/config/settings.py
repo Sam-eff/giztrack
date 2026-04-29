cab15c697e8056269b8eb3909f359982e672daaf
Sam-eff <richard.effiong2000@gmail.com>
2026-04-29 19:30:49 +0100

    Add EMAIL_USE_SSL support for Resend SMTP

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -289,6 +289,7 @@ EMAIL_BACKEND = env("EMAIL_BACKEND", default="django.core.mail.backends.console.
 EMAIL_HOST = env("EMAIL_HOST", default="smtp.gmail.com")
 EMAIL_PORT = env.int("EMAIL_PORT", default=587)
 EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", default=True)
+EMAIL_USE_SSL = env.bool("EMAIL_USE_SSL", default=False)
 EMAIL_HOST_USER = env("EMAIL_HOST_USER", default="")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default="")
 
